9f718b5e627d4beefc79c5f2e0c6828f17052caa
decarlec <decarle.c@gmail.com>
2019-09-19 21:28:09 -0400

    Added initial functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import KeypadComponent from './App.Key';
+import ResultComponent from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<KeypadComponent />, div);
+  ReactDOM.render(<ResultComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
